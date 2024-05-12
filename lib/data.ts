50a28fccc7ad37245b827738bf35cd5af71b38b3
Muhammad-Ateeb05 <160990472+Muhammad-Ateeb05@users.noreply.github.com>
2024-05-12 17:07:37 +0500

    first commit

---
+++ lib/data.ts
@@ -0,0 +1,85 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+
+import foodImg from "@/public/food.jpg"
+import ArImg from "@/public/Ar.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Internship",
+    location: "Iris,Lahore, Pakistan",
+    description:
+      "I worked as a full stack and PHP developer  at Iris, developed different projects using PHP, React, Node.js, and MongoDB.",
+    icon: React.createElement(LuGraduationCap),
+    date: "July 2023 - Aug 2021",
+  },
+ 
+] as const;
+
+export const projectsData = [
+  {
+    title: "Final Year Project",
+    description:
+      "Transform early education with AR: engaging 2 to 5-year-olds through interactive 3D models. Foster curiosity and holistic development with immersive visual learning experiences.",
+    tags: ["React", "Firebase", "MongoDB", "Tailwind"],
+    imageUrl: ArImg,
+  },
+  {
+    title: "Food Affairs",
+    description:
+      "Experience seamless event management with our website. Effortlessly create events and facilitate"+
+      " participant registration through intuitive interfaces.",
+    tags: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+    imageUrl: foodImg,
+  },
+
+ 
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "Bootstrap",
+  "Tailwind",
+  "JavaScript",
+  "React",
+  "React Native",
+  "Node.js",
+  "MongoDB",
+  "Firebase",
+  "Python",
+  "C",
+  "C++",
+  "Unity",
+  "Github",
+] as const;
