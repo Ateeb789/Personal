606ddf234ce66f1220a0ea83976bf80dec516bec
Ateeb Khan <mailtoateeb962@gmail.com>
2024-05-12 19:08:58 +0500

    first commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,10 +91,10 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/muhammad_ateeb_frontend_nextjs.pdf"
+          href="/muhammad_ateeb_frontend_nextjs-.pdf"
           download
         >
-          Download CV{" "}
+          Download CV{""}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
